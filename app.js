ddcd864669bb5320368f6d546732c13f291cddb2
SURESH KUMAR <66684186+suresh2kB@users.noreply.github.com>
2021-09-02 19:03:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const Student = require('./models/schools');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
 mongoose.connect('mongodb://localhost:27017/online-school', {
   useNewUrlParser: true,
@@ -18,6 +19,7 @@ db.once('open', () => {
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ urlencoded: true }));
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
   res.render('home');
@@ -43,7 +45,23 @@ app.post('/school', async (req, res) => {
   res.redirect(`school/${stu._id}`);
 });
 
+app.get('/school/:id/edit', async (req, res) => {
+  const student = await Student.findById(req.params.id);
+  res.render('school/edit', { student });
+});
+
+app.put('/school/:id', async (req, res) => {
+  const { id } = req.params;
+  const stu = await Student.findByIdAndUpdate(id, { ...req.body.student });
+  res.redirect(`/school/${stu._id}`);
+});
+
+app.delete('/school/:id', async (req, res) => {
+  const { id } = req.params;
+  await Student.findByIdAndDelete(id);
+  res.redirect('/school');
+});
+
 app.listen(3000, () => {
   console.log('Listining on port 3000');
 });
-
