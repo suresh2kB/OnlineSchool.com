17cbcf3bace45a885fe76000f77633831533872c
SURESH KUMAR <66684186+suresh2kB@users.noreply.github.com>
2021-09-08 08:16:14 +0530

    Update schools.js

--- models/schools.js
+++ models/schools.js
@@ -2,12 +2,23 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const students = new Schema({
+  nameofstudent: String,
+  eachstudent: [],
+});
+
+const room = new Schema({
+  name: String,
+  studentsall: [students],
+});
+
 const StudentSchema = new Schema({
   name: String,
   address: String,
   image: String,
   class: Number,
   description: String,
+  subs: [room],
   fees: Number,
 });
 
